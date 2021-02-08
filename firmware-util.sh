1d27a6595e139f68a6a90f572e8172bd0a230b16
Futurescope <34177138+Futurescope@users.noreply.github.com>
2021-02-08 14:17:01 +0800

    Update firmware-util.sh
    add local Proxy

--- firmware-util.sh
+++ firmware-util.sh
@@ -31,11 +31,11 @@ echo -e "\nDownloading supporting files..."
 rm -rf firmware.sh >/dev/null 2>&1
 rm -rf functions.sh >/dev/null 2>&1
 rm -rf sources.sh >/dev/null 2>&1
-curl -sLO ${script_url}firmware.sh
+curl -sLO -x http://192.168.31.189:7890 ${script_url}firmware.sh
 rc0=$?
-curl -sLO ${script_url}functions.sh
+curl -sLO -x http://192.168.31.189:7890 ${script_url}functions.sh
 rc1=$?
-curl -sLO ${script_url}sources.sh
+curl -sLO -x http://192.168.31.189:7890 ${script_url}sources.sh
 rc2=$?
 if [[ $rc0 -ne 0 || $rc1 -ne 0 || $rc2 -ne 0 ]]; then
 	echo -e "Error downloading one or more required files; cannot continue"
